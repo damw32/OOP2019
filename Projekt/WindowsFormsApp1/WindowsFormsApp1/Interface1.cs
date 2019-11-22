8f11a1e627567c976c514db4b3d196344c91811d
Buster_therealone <bprzybyek@gmail.com>
2019-11-22 14:18:46 +0100

    commit

---
+++ Projekt/WindowsFormsApp1/WindowsFormsApp1/Interface1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    interface IMapa
+    {
+        public void generate();//generowanie mapy na podstawie podanych wymiarów
+        public void list(); //wyśœietlenie informacji na temat każdego elementu dodanego do mapy
+        public void show(); // wyświetlenie mapy
+    }
+}
---
+++ Projekt/WindowsFormsApp1/WindowsFormsApp1/MaPA.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Mapa : IMapa
+    {
+        public int x;
+        public int y;
+        void IMapa.generate()
+        {
+            int[,] mapka = new int[this.x, this.y];
+        }
+
+        void IMapa.list()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IMapa.show()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+    
+
